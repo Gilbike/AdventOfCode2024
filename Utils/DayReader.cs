60a1f550ab4b876cc62900858f156d6b016e5896
Gilbike <marton.dikacz@gmail.com>
2024-12-01 11:01:30 +0100

    Add project files.

---
+++ Utils/DayReader.cs
@@ -0,0 +1,33 @@
+﻿namespace Utils
+{
+    public enum Days
+    {
+        Day1
+    }
+
+    public class DayReader
+    {
+        private Days day;
+
+        private string[] lines;
+
+        public DayReader(Days day)
+        {
+            this.day = day;
+
+            string path = $"./inputs/{day}.txt";
+
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException();
+            }
+
+            lines = File.ReadAllLines(path);
+        }
+
+        public string[] GetInputLines()
+        {
+            return lines;
+        }
+    }
+}
