f9c2b27ede68fc849b801b77f256bfe6c7406158
Gilbike <marton.dikacz@gmail.com>
2024-12-03 09:35:25 +0100

    day 3 part 2

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -10,11 +10,18 @@ namespace Day3
             DayReader reader = new DayReader(Days.Day3);
             string input = reader.GetInput();
 
+            Console.WriteLine("Part 1 Result:");
+            Console.WriteLine(Part1(input));
+
+            Console.WriteLine("Part 2 Result:");
+            Console.WriteLine(Part2(input));
+        }
+
+        private static int Part1(string input)
+        {
             Regex rg = new Regex(@"mul\([0-9]{1,3},[0-9]{1,3}\)");
 
             MatchCollection matchedCalculations = rg.Matches(input);
-
-            Console.WriteLine(matchedCalculations.Count);
 
             int sum = 0;
 
@@ -25,8 +32,43 @@ namespace Day3
                 sum += int.Parse(rawNumbers[0]) * int.Parse(rawNumbers[1]);
             }
 
-            Console.WriteLine("Result:");
-            Console.WriteLine(sum);
+            return sum;
+        }
+
+        private static int Part2(string input)
+        {
+            Regex rg = new Regex(@"mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don't\(\)");
+
+            MatchCollection matchedCalculations = rg.Matches(input);
+
+            int sum = 0;
+
+            bool parse = true;
+
+            foreach (Match match in matchedCalculations)
+            {
+                if (match.Value == "do()")
+                {
+                    parse = true;
+                    continue;
+                }
+                else if (match.Value == "don't()")
+                {
+                    parse = false;
+                    continue;
+                }
+
+                if (!parse)
+                {
+                    continue;
+                }
+
+                int length = match.Value.Length - 5;
+                string[] rawNumbers = match.Value.Substring(4, length).Split(',');
+                sum += int.Parse(rawNumbers[0]) * int.Parse(rawNumbers[1]);
+            }
+
+            return sum;
         }
     }
 }
