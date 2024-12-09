3b787e14dafd7c9fb05a60ec58d2fd5b3d1c0caa
Gilbike <marton.dikacz@gmail.com>
2024-12-09 23:53:27 +0100

    template

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    println!("Test Part 1 result: {}", part1());
+    println!("Part 1 result: {}", part1());
+    // println!("Test Part 2 result: {}", part2());
+    // println!("Part 2 result: {}", part2());
+}
+
+fn part1() -> usize {
+    0
+}
+
+fn get_input() {}
+fn get_test_input() {}
+
+fn parse_input(test: bool) {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_works() {
+        assert_eq!(part1(), 41)
+    }
+}
